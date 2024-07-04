c58e93c8d0cd87aaa02578142314059b0052a33f
Jin Hong Moon <jaydenmoon7@gmail.com>
2024-07-04 09:33:15 +0900

    page and hero section added

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const HeroSection  = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl font font-extrabold">Hello I'm Jayden</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi ducimus optio quasi voluptatum atque porro dolorum alias, quia similique dolor dolore veritatis necessitatibus odio quos, velit commodi deleniti adipisci quae?
+      </p>
+    </section>
+  )
+}
