915a228b463d5664e8f9ca58b5953cb78b07f479
Jin Hong Moon <jaydenmoon7@gmail.com>
2025-11-18 17:27:27 -0500

    update resume

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -12,9 +12,9 @@ const HeroSection  = () => {
           <div className="order-1 col-span-5 place-self-center mb-10">
             <div className="rounded-full  w-[250px] h-[250px] relative overflow-hidden">
                 <Image
-                src="/images/profile.jpg"
+                src="/images/profile.jpeg"
                 alt="profile image"
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 min-h-full min-w-full object-cover"
                 width={500} // trivial
                 height={500} // trivial
                 />
@@ -28,10 +28,15 @@ const HeroSection  = () => {
             <br/>
             Software & Machine Learning Engineer
           </p>
-          <div className="flex justify-center gap-4 mb-3">
+          <div className="flex flex-col sm:flex-row justify-center sm:gap-4 gap-2 mb-3 items-center">
             <button className="bg-black rounded-full px-1 py-1">
               <span className="block text-black hover:text-gray-200 bg-gray-300 hover:bg-transparent rounded-full px-5 py-2">
                 <a target="_blank" href="https://www.linkedin.com/in/jayden-moonjh/">LinkedIn</a>
+              </span>
+            </button>
+            <button className="bg-black rounded-full px-1 py-1">
+              <span className="block text-black hover:text-gray-200 bg-gray-300 hover:bg-transparent rounded-full px-5 py-2">
+                <a target="_blank" href="https://github.com/jmo-on">GitHub</a>
               </span>
             </button>
             <a
